28b6fd1512568c04faed02e2ea98488bf87e4b19
unknown <mungekar.kiran30@gmail.com>
2020-07-22 01:26:02 +0530

    TailWind css added

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+//import './App.css';
 
 
 
 import HelloWorld from './Components/HelloWorld';
 import Counter from './Components/Counter';
+import Header from './Components/Header';
 
 function App() {
   return (
     <div id="appComponent">
+        <Header />
         <HelloWorld name="World" />
         <Counter />
     </div>
